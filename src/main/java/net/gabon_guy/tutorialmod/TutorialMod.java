0419b2fdc468e8a1b4f1bb6bc56cab5d1dc19b0e
minec <minecraftcreeperpro123@gmail.com>
2024-07-09 06:41:16 -0700

    drink water and fix classes

---
+++ src/main/java/net/gabon_guy/tutorialmod/TutorialMod.java
@@ -0,0 +1,60 @@
+package net.gabon_guy.tutorialmod;
+
+import com.mojang.logging.LogUtils;
+import net.gabon_guy.tutorialmod.block.ModBlocks;
+import net.gabon_guy.tutorialmod.item.ModItems;
+import net.gabon_guy.tutorialmod.networking.ModMessages;
+import net.gabon_guy.tutorialmod.painting.ModPaintings;
+import net.gabon_guy.tutorialmod.villager.ModVillagers;
+import net.gabon_guy.tutorialmod.world.feature.ModConfiguredFeatures;
+import net.gabon_guy.tutorialmod.world.feature.ModPlacedFeatures;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TutorialMod.MOD_ID)
+public class TutorialMod {
+    public static final String MOD_ID = "tutorialmod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    // Very Important Comment
+    public TutorialMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        ModVillagers.register(modEventBus);
+        ModPaintings.register(modEventBus);
+
+        ModConfiguredFeatures.register(modEventBus);
+        ModPlacedFeatures.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            ModVillagers.registerPOIs();
+        });
+
+        ModMessages.register();
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
